6752163c9a966ee9a4479d49b037d2dc94d7e25b
Bozhidar Baltadzhiev <73103322+BojidarVn@users.noreply.github.com>
2021-03-02 16:57:28 +0200

    More advanced security configuration

---
+++ src/main/java/bg/softuni/musicdb/service/impl/MusicDBUserService.java
@@ -0,0 +1,54 @@
+package bg.softuni.musicdb.service.impl;
+
+import bg.softuni.musicdb.model.entity.UserEntity;
+import bg.softuni.musicdb.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class MusicDBUserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public MusicDBUserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        UserEntity userEntity = userRepository.findByName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User with username " + username + " was not found"));
+
+        return mapToUserDetails(userEntity);
+    }
+
+    private UserDetails mapToUserDetails(UserEntity userEntity) {
+
+        List<GrantedAuthority> authorities =
+                userEntity
+                        .getRoles()
+                        .stream()
+                        .map(r -> new SimpleGrantedAuthority(r.getRole().name()))
+                        .collect(Collectors.toList());
+
+
+        UserDetails result = new User(
+                userEntity.getName(),
+                userEntity.getPassword(),
+                authorities);
+
+        return result;
+    }
+
+
+}
